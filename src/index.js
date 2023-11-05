6225ab19669069f21b09d767a8534f3e7b4e6c11
Gabriela Lima <gwabriel@gmail.com>
2023-11-05 01:10:27 -0300

    Adicionando a configuração do Usuário

--- src/index.js
+++ src/index.js
@@ -48,3 +48,42 @@ app.listen(port, () => {
     mongoose.connect('mongodb+srv://caiohenrique:qEQmnXxijZjcVFMa@api-receitas.eug3rkp.mongodb.net/?retryWrites=true&w=majority');
   console.log(`Loading ${port}`)
 }) 
+
+const Usuarios = mongoose.model('Usuarios', { 
+    nome: String,
+    data_nascimento: Date,
+    email: String
+});
+
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await Usuarios.find()
+    return res.send(usuarios)
+})
+
+app.delete("/usuarios/:id", async(req, res) =>{
+    const usuario = await Usuarios.findByIdAndRemove(req.params.id)
+    return res.send(usuario)
+})
+
+app.put("/usuarios/:id", async(req, res) => {
+    const usuario = await Usuarios.findByIdAndUpdate(req.params.id, {
+        nome: req.body.nome,
+        data_nascimento: req.body.data_nascimento,
+        email: req.body.email
+    }, {
+        new:true
+    })
+
+    return res.send(usuario)
+})
+
+app.post("/usuarios", async (req, res) => {
+    const usuario = new Usuarios({
+        nome: req.body.nome,
+        data_nascimento: req.body.data_nascimento,
+        email: req.body.email
+    })
+
+    await usuario.save()
+    return res.send(usuario)
+})
